453e6b88afe28989871b68e7b24a7a3883a0ee86
wamapple <wamapple@163.com>
2012-06-29 13:03:13 +0800

    first commit

---
+++ WamTabBar/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  WamTabBar
+//
+//  Created by MagicStudio on 12-5-17.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
